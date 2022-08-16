698af07296446d527dfcec40e1e0de3fe4c5bf45
Alessandro <alessandrotambellini03@gmail.com>
2022-08-16 15:39:00 +0200

    show output in dec and exp form

--- intermediary.js
+++ intermediary.js
@@ -1,10 +1,19 @@
-const btn = document.querySelector("button");
-const inputField = document.querySelector("input");
+const btn = document.querySelector("main > button");
+const inputField = document.querySelector("main > input");
 const outputField = document.querySelector("p");
 
 btn.addEventListener("click", () => {
-  const factorial = calc_factorial(parseInt(inputField.value, 10));
-  outputField.appendChild(document.createTextNode(factorial));
+  // f stands for factorial
+  const fDec = calc_factorial(parseInt(inputField.value, 10));
+  const fExp = expCast(fDec, fDec.length - 1);
+  /*   const fHex = hexCast(fDec);
+  const fBin = binCast(fDec);
+ */
+  document.querySelector("#dec-td").appendChild(document.createTextNode(fDec));
+  document.querySelector("#exp-td").appendChild(document.createTextNode(fExp));
+  /*   document.querySelector("#hex-td").appendChild(document.createTextNode(fHex));
+  document.querySelector("#bin-td").appendChild(document.createTextNode(fBin));
+ */
 });
 
 function calc_factorial(num) {
@@ -14,6 +23,34 @@ function calc_factorial(num) {
     ["number"],
     [num]
   );
-  console.log(reversedFactorial);
   return reversedFactorial.split("").reverse().join("");
 }
+
+function expCast(num, fractionDigits) {
+  return Number.parseFloat(num).toExponential(
+    fractionDigits < 10 ? fractionDigits : 9
+  );
+}
+
+/* function hexCast(num) {
+  //return Number.parseFloat(num).toString(16);
+
+} */
+
+/* function binCast(num) {
+  let numBits = (parseInt(num, 10) >>> 0).toString(2);
+  const numBitsLength = numBits.length;
+  let nibblesStr = "";
+  numBits = numBits.padStart(
+    numBitsLength > 4 ? numBitsLength + (4 - (numBitsLength % 4)) : 4,
+    "0"
+  );
+  for (let i = 1; i <= numBits.length; i++) {
+    nibblesStr += numBits[i - 1];
+    if (i % 4 === 0) {
+      nibblesStr += " ";
+    }
+  }
+
+  return nibblesStr;
+} */
